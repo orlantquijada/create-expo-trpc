64982212a79934a1b141906929e2b627b706d519
Orlan Quijada <orlantquijada@gmail.com>
2023-03-21 12:41:09 +0800

    chore(config): import alias

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,7 +1,20 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true)
   return {
-    plugins: ["nativewind/babel"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module-resolver",
+        {
+          alias: {
+            // This needs to be mirrored in tsconfig.json
+            "~": "./src",
+            "@assets": "./assets",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
     presets: ["babel-preset-expo"],
-  };
-};
+  }
+}
