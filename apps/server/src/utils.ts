960e703fa96cd3e14d90c77d2f7f281328abbbb0
Orlan Quijada <orlantquijada@gmail.com>
2023-03-22 04:21:57 +0800

    chore: deployable api config

--- apps/server/src/utils.ts
+++ apps/server/src/utils.ts
@@ -6,12 +6,14 @@ import os from "os"
  */
 export function getHostIP() {
   const interfaces = os.networkInterfaces()
-  const ipv4Interface = Object.values(interfaces).find((intf) =>
-    intf?.some(
-      (e) =>
-        e.family === "IPv4" && !e.internal && e.address.startsWith("192.168."),
-    ),
-  )
+  const ipv4Interface = Object.values(interfaces)
+    .flat()
+    .find(
+      (intf) =>
+        intf?.family === "IPv4" &&
+        !intf.internal &&
+        intf.address.startsWith("192.168"),
+    )
 
-  return ipv4Interface?.[0]?.address ?? ""
+  return ipv4Interface?.address ?? ""
 }
